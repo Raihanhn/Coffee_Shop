ec58faec168617a60189325a5943440caefe9f01
raihanhn <raihanhn20@gmail.com>
2023-01-07 20:02:54 +0600

    4th commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import img1 from "../../src/assets/image/home-img-1.png"
+import img2 from "../../src/assets/image/home-img-2.png"
+import img3 from "../../src/assets/image/home-img-3.png"
 
 const Home = () => {
   return (
@@ -14,6 +16,13 @@ const Home = () => {
                     <img src={img1} alt="" className='main-home-image' />
                 </div>
             </div>
+
+            <div className="image-slider">
+                <img src={img1} alt="" />
+                <img src={img2} alt="" />
+                <img src={img3} alt="" />
+            </div>
+
         </section>
     </div>
   )
