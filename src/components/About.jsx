c88c0c2bad22c879cd48fa99ce23d062fc827930
raihanhn <raihanhn20@gmail.com>
2023-01-09 00:46:31 +0600

    6th commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import img1 from "../../src/assets/image/about-img.png"
+import img2 from "../../src/assets/image/about-icon-1.png"
+import img3 from "../../src/assets/image/about-icon-2.png"
+import img4 from "../../src/assets/image/about-icon-3.png"
 
 const About = () => {
   return (
@@ -9,6 +12,25 @@ const About = () => {
             <div className="row">
                 <div className="image">
                     <img src={img1} alt="" />
+                </div>
+                <div className="content">
+                  <h3 className="title">what's make our coffee special!</h3>
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id perferendis dolores, atque tenetur laboriosam error!</p>
+                  <a href="#" className='btn' >Read more</a>
+                  <div className="icons-container">
+                    <div className="icons">
+                      <img src={img2} alt="" />
+                      <h3>quality coffee</h3>
+                    </div>
+                    <div className="icons">
+                      <img src={img3} alt="" />
+                      <h3>our branches</h3>
+                    </div>
+                    <div className="icons">
+                      <img src={img4} alt="" />
+                      <h3>free delivery</h3>
+                    </div>
+                  </div>
                 </div>
             </div>
         </section>
