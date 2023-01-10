36e7e99a731a6feeba77f29883ebb232eb790056
raihanhn <raihanhn20@gmail.com>
2023-01-11 04:30:28 +0600

    11th commit

--- src/components/menus.js
+++ src/components/menus.js
@@ -3,49 +3,49 @@ import img2 from "../assets/image/menu-2.png"
 import img3 from "../assets/image/menu-3.png"
 import img4 from "../assets/image/menu-4.png"
 import img5 from "../assets/image/menu-5.png"
-import img5 from "../assets/image/menu-6.png"
+import img6 from "../assets/image/menu-6.png"
 
 const menus = [
 
     {
         id:1,
         title:"our special coffee",
-        desc:"coffee is very healthy and refreshing mind ",
+        desc:"coffee is very healthy and refreshing our mind ",
         price :"8.10",
         image:img1
     },
     {
         id:2,
         title:"our special coffee",
-        desc:"coffee is very healthy and refreshing mind",
+        desc:"coffee is very healthy and refreshing our mind",
         price :"10.20",
         image:img2
     },
     {
         id:3,
         title:"our special coffee",
-        desc:"coffee is very healthy and refreshing mind",
+        desc:"coffee is very healthy and refreshing our mind",
         price :"15.20",
         image:img3
     },
     {
         id:4,
         title:"our special coffee",
-        desc:" coffee is very healthy and refreshing mind ",
+        desc:" coffee is very healthy and refreshing our mind ",
         price :"5.99",
         image:img4
     },
     {
         id:5,
         title:"our special coffee",
-        desc:"coffee is very healthy and refreshing mind",
+        desc:"coffee is very healthy and refreshing our mind",
         price :"7.20",
         image:img5
     },
     {
         id:6,
         title:"our special coffee",
-        desc:"coffee is very healthy and refreshing mind",
+        desc:"coffee is very healthy and refreshing our mind",
         price :"10.00",
         image:img6
     }
