629b523fad0a0fb195fef4d0e9c62edaf471dd1a
raihanhn <raihanhn20@gmail.com>
2023-01-10 20:43:57 +0600

    9th commit

---
+++ src/components/Menu.js
@@ -0,0 +1,43 @@
+
+
+const Menu = [
+
+    {
+        id:1,
+        title:"man shirt",
+        desc:"upto 30% off",
+        price :"10.20",
+        image:img1
+    },
+    {
+        id:2,
+        title:"man shirt",
+        desc:"upto 30% off",
+        price :"10.20",
+        image:img2
+    },
+    {
+        id:3,
+        title:"man shirt",
+        desc:"upto 30% off",
+        price :"10.20",
+        image:img3
+    },
+    {
+        id:4,
+        title:"man shirt",
+        desc:"upto 30% off",
+        price :"10.20",
+        image:img4
+    },
+    {
+        id:5,
+        title:"man shirt",
+        desc:"upto 30% off",
+        price :"10.20",
+        image:img5
+    },
+
+]
+
+export default Menu;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <div className='aka' >
+        <section className="menu" id="menu">
+            <h1 className="heading">our menu <span>popular menu</span></h1>
+
+            <div className="box-container">
+
+              <a href="#" className='box'>
+                <img src="" alt="" />
+                <div className="content">
+                  <h3>our special coffee</h3>
+                  <p>Lorem ipsum dolor sit amet.</p>
+                  <span>$9.00</span>
+                </div>
+              </a>
+
+
+
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default Menu
